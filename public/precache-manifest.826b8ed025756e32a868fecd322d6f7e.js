01fd0daa9ee226ad0395ff2b02088038dd66c698
matan1542 <matan8380@gmail.com>
2021-07-08 09:39:18 +0300

    public

---
+++ public/precache-manifest.826b8ed025756e32a868fecd322d6f7e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95fabb11e140691e0611ce6972e41915",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d58f41191b3f8063de3",
+    "url": "/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "129325512243c37e8762",
+    "url": "/static/css/main.0fa7ec49.chunk.css"
+  },
+  {
+    "revision": "3d58f41191b3f8063de3",
+    "url": "/static/js/2.02dbac9d.chunk.js"
+  },
+  {
+    "revision": "129325512243c37e8762",
+    "url": "/static/js/main.d2f55631.chunk.js"
+  },
+  {
+    "revision": "6b4a89c18408cdb0a7d1",
+    "url": "/static/js/runtime~main.a38a3816.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
