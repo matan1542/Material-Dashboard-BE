655c9aa2a7ad519022c885d4de0d3a5c93ff4833
matan1542 <matan8380@gmail.com>
2021-07-08 03:34:05 +0300

    public

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(session)
 console.log('process.env.NODE_ENV',process.env.NODE_ENV)
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.resolve('public')))
+    app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
     const corsOptions = {
         origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
@@ -40,8 +40,8 @@ app.use('/api/user', userRoutes)
 // Make every server-side-route to match the index.html
 // so when requesting http://localhost:3030/index.html/car/123 it will still respond with
 // our SPA (single page app) (the index.html file) and allow vue/react-router to take it from there
-app.get('*', (req, res) => {
-    res.sendFile('index.html', { path })
+app.get('/**', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
 
 const logger = require('./services/logger.service')
